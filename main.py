d685e9a986b830891c70f5b5e198dff0df51f718
sdf299 <quocbao0905461606@gmail.com>
2024-01-22 07:29:33 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+import smtplib
+import os
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from wtforms.validators import InputRequired, NumberRange
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+
+ckeditor = CKEditor(app)
+
+
+# class Donation(FlaskForm):
+#     Email = StringField('Email')
+#     Name = StringField('Name')
+#     Amount = IntegerField('Amount', validators=[
+#         InputRequired(),
+#         NumberRange(min=0, message='Value must be between 0 and 100')])
+#     feedback = CKEditorField('Any Feedback?')
+
+
+# class Donor(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(100), unique=True, index=True)
+#     name = db.Column(db.String(100), index=True)
+#     donated = db.Column(db.Integer, nullable=False)
+#     feedback = db.Column(db.String(250))
+#     method = db.Column(db.String,nullable=False)
+
+# class PostForm(FlaskForm):
+#     title = StringField('Blog Post Title')
+#     subtitle = StringField('Subtitle')
+#     author = StringField('Author')
+#     img_url = URLField('Img_url')
+#     body = CKEditorField('Body')
+#     submit = SubmitField('Submit')
+
+
+
+@app.route('/')
+def about2():
+    return render_template('about_2.html')
+
+
+@app.route('/web')
+def web():
+    return render_template('web.html')
+
+
+@app.route('/donate', methods=["GET", "POST"])
+def donate():
+    if request.method == "POST":
+        with open('customer.txt', 'a', encoding='utf-8') as file:
+            user_amount = request.form.get('flexRadioDefault')
+            user_payment = request.form.get('DonationPayment')
+            user_feedback = request.form.get('feedback')
+            user_name = request.form.get('donation-name')
+            user_email = request.form.get('donation-email')
+            new_infor = [
+                user_email,
+                user_name,
+                user_amount,
+                user_feedback,
+                user_payment
+            ]
+            file.write(f'{new_infor}\n')
+        # db.session.add(new_infor)
+        # db.session.commit()
+        return redirect(url_for('about2'))
+
+    return render_template('donate2.html')
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+MAIL_ADDRESS = 'bao.nguyenquoc05@truongvietanh.com'
+MAIL_APP_PW = 'oqqw tpuz icvx fzkq'
+TO_MAIL = 'quocbao0905461606@gmail.com'
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MAIL_ADDRESS, MAIL_APP_PW)
+        connection.sendmail(
+            from_addr=MAIL_ADDRESS,
+            to_addrs=TO_MAIL,
+            msg=email_message
+        )
+
+
+@app.route('/colab')
+def colab():
+    return render_template('colab.html')
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
