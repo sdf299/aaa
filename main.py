ff30215df1bc953b47b4910569209d4d756e143b
sdf299 <quocbao0905461606@gmail.com>
2024-01-22 08:26:06 +0700

    import environ

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///information.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///volunteer.db")
 db = SQLAlchemy()
 db.init_app(app)
 ckeditor = CKEditor(app)
@@ -126,4 +126,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5003)
+    app.run(debug=False)
